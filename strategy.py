f697b9fa96aa4ae4218081182d6cc5377da0f6fa
ravshantorayev105-alt <ravshantorayev105@gmail.com>
2026-03-14 12:13:51 -0300

    Remove fvg function from strategy.py
    Removed the fvg function and its logic from strategy.py.

--- strategy.py
+++ strategy.py
@@ -6,14 +6,4 @@ def liquidity(df):
     if df.high.iloc[-1] > df.high.iloc[-2]:
         return "SELL"
 
-
-def fvg(df):
-
-    c1=df.iloc[-3]
-    c3=df.iloc[-1]
-
-    if c1.high < c3.low:
-        return "BUY"
-
-    if c1.low > c3.high:
-        return "SELL"
+    return None
