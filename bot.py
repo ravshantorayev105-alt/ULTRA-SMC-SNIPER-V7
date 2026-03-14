a402ae9ef120a89c8633fdf24dab7b6dd5d9e78b
ravshantorayev105-alt <ravshantorayev105@gmail.com>
2026-03-14 10:50:24 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,33 @@
+import asyncio
 from telegram import Bot
-from config import TOKEN,CHAT_ID
+from config import TOKEN, CHAT_ID
 
 bot = Bot(token=TOKEN)
 
-async def send_signal(text,img):
-
-    await bot.send_photo(
+async def send_signal(text):
+    await bot.send_message(
         chat_id=CHAT_ID,
-        photo=open(img,"rb"),
-        caption=text
+        text=text
     )
+
+async def main():
+
+    while True:
+
+        text = """
+ULTRA SMC SNIPER V7
+
+PAIR: BTC/USD
+TYPE: BUY
+
+ENTRY: 65000
+SL: 64500
+TP: 66500
+RR: 1:3
+"""
+
+        await send_signal(text)
+
+        await asyncio.sleep(300)  # 5 minut
+
+asyncio.run(main())
